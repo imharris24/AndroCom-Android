03801b0a89231a24c154f1e6c209c9f636905edd
Muhammad Harris <harris20014@gmail.com>
2024-07-16 12:36:09 +0500

    update release

--- Source Code/Android App Environment/androcom/app/build.gradle.kts
+++ Source Code/Android App Environment/androcom/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.application.androcom"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
